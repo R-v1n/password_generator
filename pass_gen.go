a86bab24b9a9c52543bbb484d09895c97ff30597
R-v1n <76698141+R-v1n@users.noreply.github.com>
2021-05-23 22:13:04 +0530

    Add files via upload
    alternative_method

---
+++ pass_gen.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+var perm_count uint64
+
+func swap(per_text string, init, final int) string {
+	swap_text := []rune(per_text)
+	swap_text[init], swap_text[final] = swap_text[final], swap_text[init]
+	per_text = string(swap_text)
+	return per_text
+}
+
+func permute(per_text string, init, final int) {
+	if init == final {
+		fmt.Println(per_text)
+		perm_count += 1
+		return
+	}
+	for i := init; i <= final; i++ {
+		per_text = swap(per_text, init, i)
+		permute(per_text, init+1, final)
+	}
+}
+
+func main() {
+	var text string
+	text = "ABCDEFGHI"
+	final := len([]rune(text)) - 1
+	permute(text, 0, final)
+	fmt.Printf("total permutation count :: >> %d ", perm_count)
+}
